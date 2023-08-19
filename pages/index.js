83d38b72f8bc87aae020f3a47b01df677fc6a529
David Poole <dbpoole7@gmail.com>
2023-08-19 16:18:03 -0500

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+/* eslint-disable react/no-unescaped-entities */
+// import { useRouter } from 'next/router';
+// import { Button, Image, Carousel } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
+import Head from 'next/head';
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
-
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  // const router = useRouter();
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <Head>
+        <title>Home Profile</title>
+      </Head>
+      <div className="homepage-desc-text">
+        <h3>This is where the home/profile page contents will go.</h3>
+      </div>
+    </>
   );
 }
 
